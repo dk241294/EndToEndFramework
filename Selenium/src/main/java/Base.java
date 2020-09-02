63f6d95ec862040e58875361ab4dc6d558538add
Deepak Kumar <deepak24121994@gmail.com>
2020-09-02 23:10:04 +0530

    base class

---
+++ Selenium/src/main/java/Base.java
@@ -0,0 +1,34 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class Base {
+    WebDriver driver;
+
+    public WebDriver initializeDriver() throws IOException {
+
+
+        Properties prop = new Properties();
+        FileInputStream fs = new FileInputStream("C:\\Users\\DEEPAK\\Documents\\GitHub\\EndToEndFramework\\Selenium\\src\\main\\dataProperties");
+        prop.load(fs);
+        String browserName = prop.getProperty("browser");
+        if (browserName == "chrome") {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+            driver.get("");
+
+        } else if (browserName == "firefox") {
+            WebDriverManager.chromedriver().setup();
+            driver = new FirefoxDriver();
+            driver.get("");
+        }
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        return driver;
+    }
+}
