c277e9f372e8c04902fed6b6bdf530ff2cd48b2a
Deepak Kumar <deepak24121994@gmail.com>
2020-09-03 13:46:57 +0530

    validate navigtion bar

---
+++ Selenium/src/test/java/ValidateNAvigationBar.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pageObject.LandingPage;
+
+import java.io.IOException;
+
+public class ValidateNAvigationBar extends Base {
+    WebDriver driver;
+    @Test
+    public void validate()  throws IOException {
+        // Base b= new Base(driver);
+        driver=initializeDriver();
+        driver.get("http://www.qaclickacademy.com/");
+        LandingPage lp=new LandingPage(driver);
+        //lp.getTitle();
+       // Assert.assertEquals(lp.getTitle().getText(),"FEATURED COURSES");
+        Assert.assertTrue(lp.getnavigationBar().isDisplayed());
+    }
+}
