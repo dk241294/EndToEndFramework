f59ba2de001939191f298e9d3bd57089b19a9983
Deepak Kumar <deepak24121994@gmail.com>
2020-09-03 11:58:40 +0530

    data provider annotation

--- Selenium/src/main/java/HomePage.java
+++ Selenium/src/main/java/HomePage.java
@@ -1,3 +1,4 @@
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pageObject.LandingPage;
 import pageObject.LoginPage;
@@ -7,18 +8,26 @@ import java.io.IOException;
 public class HomePage extends Base {
 
 
-    @Test
-    public void homePageNavigation() throws IOException {
-
+    @Test(dataProvider = "getData")
+    public void homePageNavigation(String username,String Password) throws IOException {
         driver = initializeDriver();
         driver.get("http://www.qaclickacademy.com/");
         LandingPage lp = new LandingPage(driver);
         lp.getLogin().click();
         LoginPage l = new LoginPage(driver);
-        l.getUserNAme().sendKeys("deepak");
-        l.getUserpassword().sendKeys("password");
+        l.getUserNAme().sendKeys(username);
+        l.getUserpassword().sendKeys(Password);
         l.getsubmit().click();
 
+    }
 
+    @DataProvider
+    public Object[] getData() {
+        Object[][] data = new Object[2][2];
+        data[0][0] = "deepak kumar";
+        data[0][1] = "kumar";
+        data[1][0] = "sallu";
+        data[1][1] = "khan";
+        return data;
     }
 }
